09066c1d241843ce503039973cda67b76d96f3b3
Emmanuel Chukwu <officialecho95@outlook.com>
2023-04-06 23:40:17 +0000

    fix my code

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,28 +1,34 @@
 #!/usr/bin/python3
-
+'''
+a module that computes the area and param of a square
+'''
 class square():
-    
-    width = 0
-    height = 0
+   """the containing class"""
 
-    
-    def __init__(self, *args, **kwargs):
-        for key, value in kwargs.items():
-            setattr(self, key, value)
+   width = 0
+   height = 0
 
-    def area_of_my_square(self):
-        """ Area of the square """
-        return self.width * self.width
 
-    def PermiterOfMySquare(self):
-        return (self.width * 2) + (self.height * 2)
+   def __init__(self, *args, **kwargs):
+       '''initialization'''
+       for key, value in kwargs.items():
+           setattr(self, key, value)
 
-    def __str__(self):
-        return "{}/{}".format(self.width, self.height)
+   def area_of_my_square(self):
+       """ Area of the square """
+       return self.width * self.width
+
+   def PermiterOfMySquare(self):
+       '''the param calculator'''
+       return (self.width * 2) + (self.height * 2)
+
+   def __str__(self):
+       '''the output'''
+       return "{}/{}".format(self.width, self.height)
 
 if __name__ == "__main__":
 
-    s = square(width=12, height=9)
-    print(s)
-    print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+   s = square(width=12, height=9)
+   print(s)
+   print(s.area_of_my_square())
+   print(s.PermiterOfMySquare())
